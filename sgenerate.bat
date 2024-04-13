838bd430dfd7ed848c7e5cbd84f2860284b791c9
AvecLeSSH <sadeck.brahima@etu.univ-grenoble-alpes.fr>
2024-04-13 16:38:42 +0200

    Ajout fichier sgenerate.bat

---
+++ sgenerate.bat
@@ -0,0 +1,13 @@
+mysql -u root -e "drop database bloglaravel"
+mysql -u root -e "create database bloglaravel"
+mysql -u root bloglaravel < database/blog.sql
+
+
+rm -rf database/migrations/*
+rm -rf app/Models/*
+rm -rf database/factories/*
+php artisan migrate:refresh
+php artisan migrate:generate --ignore="personal_access_tokens" --squash
+php artisan code:models
+php artisan generate:factory
+php artisan migrate:refresh --seed
